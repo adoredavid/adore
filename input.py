6718c1dd1918ef545422b0ea9a7e573f9ff3e754
adore <fetion328467833@live.com>
2020-04-29 20:00:22 +0800

    向你提交

---
+++ input.py
@@ -0,0 +1,24 @@
+# print(type(4545))
+# print(type(53.55))
+# print(type(True))
+# print(type(())
+# print(type([]))
+# print(type({}))
+
+
+# a = str(44545)
+# print(type(a))
+
+
+# a = float( input ("請輸入："))
+# b = float( input ("請輸入："))
+# print ( "input獲取的内容",a + b )
+
+
+
+# a = 'e呵 呵e'
+# print(len(a))
+
+a = input ("你今天吃了嗎？答：")
+b = input ("我吃了！你呢，你今天吃了幾頓飯？答：")
+print('問：他們總共説了多少個字',len(a +b ))
